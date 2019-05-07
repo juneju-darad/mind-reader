4a4dfe163e122214cc2940283c4a6e239f176451
Juneju Darad <djuneju@gmail.com>
2019-05-08 00:01:09 +0600

    Game Added

---
+++ main.c
@@ -0,0 +1,70 @@
+#include<stdio.h>
+
+int main(void)
+{
+      char x,y;
+      printf("Think of a number between 2 to 9\n");
+      scanf("%c", &y);
+      printf("Now multiply the number by 9\n");
+      scanf("%c", &y);
+      printf("You found two digit!\nNow add this two digit.\nlike 26! 2+6=8\n");
+      scanf("%c", &y);
+      printf("then Add 42 with that number\n");
+      scanf("%c", &y);
+      printf("then subtract 16 from that number\n");
+      scanf("%c", &y);
+      printf("then divide that number by 5\n");
+      scanf("%c", &y);
+      printf("multiply that number by itself\n");
+      scanf("%c", &y);
+      printf("Look at me & think the number again!!\n");
+      scanf("%c", &y);
+      printf("Now you have 49 !!\n");
+      scanf("%c", &y);
+      printf("Wanna play again ??\nif yes then enter Y or enter N\n");
+      scanf("%c", &y);
+      if (y=='y' || y=='Y') {
+            printf("Think of a number between 2 to 9\n");
+            scanf("%c", &y);
+            printf("Now multiply the number by 9\n");
+            scanf("%c", &y);
+            printf("You found two digit!\nNow add this two digit.\nlike 26! 2+6=8\n");
+            scanf("%c", &y);
+            printf("multiply that number by itself\n");
+            scanf("%c", &y);
+            printf("then subtract 15 from that number\n");
+            scanf("%c", &y);
+            printf("then divide that number by 3\n");
+            scanf("%c", &y);
+            printf("then Add 50 with that number\n");
+            scanf("%c", &y);
+            printf("Look at me & think the number again!!\n");
+            scanf("%c", &y);
+            printf("Now you have 72 !!\n");
+            scanf("%c", &y);
+            printf("Wanna play again ??\nif yes then enter Y or enter N\n");
+            scanf("%c", &y);
+            if (y=='y' || y=='Y') {
+                  printf("Think of a number between 2 to 9\n");
+                  scanf("%c", &y);
+                  printf("Now multiply the number by 9\n");
+                  scanf("%c", &y);
+                  printf("You found two digit!\nNow add this two digit.\nlike 26! 2+6=8\n");
+                  scanf("%c", &y);
+                  printf("multiply that number by 7\n");
+                  scanf("%c", &y);
+                  printf("then Add 17 with that number\n");
+                  scanf("%c", &y);
+                  printf("then divide that number by 4\n");
+                  scanf("%c", &y);
+                  printf("then subtract 40 from that number\n");
+                  scanf("%c", &y);
+                  printf("Look at me & think the number again!!\n");
+                  scanf("%c", &y);
+                  printf("Now you have -20 !!\n");
+                  scanf("%c", &y);
+            }
+      }
+      else
+      return 0;
+}
